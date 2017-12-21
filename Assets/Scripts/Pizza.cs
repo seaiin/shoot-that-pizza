5db453bb60ee40d941b0cee2459b77e0e3506227
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2017-12-21 14:26:55 +0700

    add pizza & move script file to Scripts folder

---
+++ Assets/Scripts/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pizza : MonoBehaviour {
+    private Dictionary<int, string> typePizza = new Dictionary<int, string>();
+
+    void Start() {
+        typePizza.Add(1, "HWI");
+        typePizza.Add(2, "ITM");
+        typePizza.Add(3, "PRM");
+        typePizza.Add(4, "MLV");
+        typePizza.Add(5, "BBQ");
+    }
+
+    public string getTypePizza () {
+        int numType = (int) Random.Range(0, 6);
+
+        return typePizza[numType];
+    }
+}
