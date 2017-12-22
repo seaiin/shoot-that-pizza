e11d816f9c12e3c6a0549dbd74b24bf0b709c644
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2017-12-22 15:21:08 +0700

    use object pooling for house

---
+++ Assets/Scripts/GenerateHouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateHouse : MonoBehaviour {
+    public GameObject housePrefab;
+
+	// Use this for initialization
+	void Start () {
+        //for (int i = 0; i < 20; i++) {
+        Invoke("houseMove", 4);
+        Invoke("houseMove", 2);
+        Invoke("houseMove", 1);
+        //}
+    }
+	
+	void houseMove () {
+        GameObject house = ObjectPooling.SharedInstance.GetPooledObject();
+
+        if (house != null) {
+            house.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour {
+
+    public static ObjectPooling SharedInstance;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+
+    void Awake () {
+        SharedInstance = this;
+    }
+
+    void Start () {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++) {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject() {
+        //1 iterate pooled objects
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            //2 check item in list that not active currently
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        //3 if there are no inactive object. Exit method and return nothing
+        return null;
+    }
+}
