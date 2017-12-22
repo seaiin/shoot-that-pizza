0f591ed6852dbc17ef7fa2f18c5039647e22d8ca
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2017-12-22 17:51:11 +0700

    check pizza code when objects come in scene

---
+++ Assets/CheckPizzaCode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPizzaCode : MonoBehaviour {
+    public GameManage gameManage;
+
+    void OnTriggerEnter2D (Collider2D collision) {
+        if (collision.gameObject.tag == "House") {
+            //collision.gameObject.SendMessage("getPizza");
+            HouseMovement house = collision.gameObject.GetComponent<HouseMovement>();
+            string pizza = house.getPizza();
+            Debug.Log("pizza: " + pizza);
+
+            //gameManage.SendMessage("showPizza", pizza); // Send data pass by method in GameManage
+        }
+    }
+}
