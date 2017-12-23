812039d8143a3b8a78f65e397705c3f75acfbf9c
seaiin <namefinal@gmail.com>
2017-12-23 20:49:52 +0700

    Clean cod and move some obj

--- Assets/Scripts/GameManage.cs
+++ Assets/Scripts/GameManage.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class GameManage : MonoBehaviour {
 
-    private TypingPizza typingPizza;
-    private Score score;
-    private Truck truck;
-    private HouseMovement house;
+    public TypingPizza typingPizza;
+    public Score score;
+    public Truck truck;
+
     private string pizza = "";
 
 	// Use this for initialization
 	void Start () {
-        typingPizza = gameObject.AddComponent<TypingPizza>();
-        score = gameObject.AddComponent<Score>();
-        truck = gameObject.AddComponent<Truck>();
+
         typingPizza.SetPizza(pizza);
+
     }
 	
 	// Update is called once per frame
@@ -30,16 +29,4 @@ public class GameManage : MonoBehaviour {
         }
 
 	}
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "House")
-        {
-            collision.gameObject.SendMessage("ReadyToType");
-            house = collision.gameObject.GetComponent<HouseMovement>();
-            pizza = house.getPizza();
-            typingPizza.SetPizza(pizza);
-            Debug.Log(pizza);
-        }
-    }
 }
