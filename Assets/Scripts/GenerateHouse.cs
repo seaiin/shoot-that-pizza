7187bb94a7377f50d1ff91872a77846ac031ad18
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2017-12-25 15:33:19 +0700

    change branch master to dev

--- Assets/Scripts/GenerateHouse.cs
+++ Assets/Scripts/GenerateHouse.cs
@@ -9,10 +9,17 @@ public class GenerateHouse : MonoBehaviour {
 
     private float time;
     private float spawnTime;
+    private static Dictionary<int, string> tagHouse = new Dictionary<int, string>();
+    private string tagString = "House1";
 
-	void Start () {
+    void Start () {
         setRandomTime();
         time = minTime;
+        tagHouse.Add(0, "House1");
+        tagHouse.Add(1, "House2");
+        tagHouse.Add(2, "House3");
+        tagHouse.Add(3, "House4");
+        tagHouse.Add(4, "House5");
     }
 
     void FixedUpdate () {
@@ -28,8 +35,10 @@ public class GenerateHouse : MonoBehaviour {
 
     void houseSpawn () {
         time = 0;
+        int tagNum = (int)(Random.Range(0, 5));
+        tagString = tagHouse[tagNum];
 
-        GameObject house = ObjectPooling.SharedInstance.GetPooledObject();
+        GameObject house = ObjectPooling.SharedInstance.GetPooledObject(tagString);
         //HouseMovement houseObject = house.GetComponent<HouseMovement>();
         //houseObject.showType();
 
--- Assets/Scripts/ObjectPooling.cs
+++ Assets/Scripts/ObjectPooling.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class ObjectPoolItem {
+    public int amountToPool;
+    public GameObject objectToPool;
+    public bool shouldExpand;
+}
+
 public class ObjectPooling : MonoBehaviour {
 
     public static ObjectPooling SharedInstance;
+    public List<ObjectPoolItem> itemsToPool;
     public List<GameObject> pooledObjects;
-    public GameObject objectToPool;
-    public int amountToPool;
-    public bool shouldExpand = true; // can expand when run out of house
 
     void Awake () {
         SharedInstance = this;
@@ -16,32 +21,35 @@ public class ObjectPooling : MonoBehaviour {
 
     void Start () {
         pooledObjects = new List<GameObject>();
-        for (int i = 0; i < amountToPool; i++) {
-            GameObject obj = (GameObject)Instantiate(objectToPool);
-            obj.SetActive(false);
-            pooledObjects.Add(obj);
+        foreach (ObjectPoolItem item in itemsToPool) {
+            for (int i = 0; i < item.amountToPool; i++) {
+                GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
         }
     }
 
-    public GameObject GetPooledObject() {
+    public GameObject GetPooledObject(string tag) {
         //1 iterate pooled objects
-        for (int i = 0; i < pooledObjects.Count; i++)
-        {
+        for (int i = 0; i < pooledObjects.Count; i++) {
             //2 check item in list that not active currently
-            if (!pooledObjects[i].activeInHierarchy)
-            {
+            if (!pooledObjects[i].activeInHierarchy && pooledObjects[i].tag == tag) {
                 //Debug.Log("have inactive object: " + i);
                 return pooledObjects[i];
             }
         }
         //3 if there are no inactive object. Exit method and return nothing
-        if (shouldExpand) {
-            GameObject obj = (GameObject)Instantiate(objectToPool);
-            obj.SetActive(false);
-            pooledObjects.Add(obj);
-            return obj;
-        } else {
-            return null;
+        foreach (ObjectPoolItem item in itemsToPool) {
+            if (item.objectToPool.tag == tag) {
+                if (item.shouldExpand) {
+                    GameObject obj = (GameObject)Instantiate(item.objectToPool);
+                    obj.SetActive(false);
+                    pooledObjects.Add(obj);
+                    return obj;
+                }
+            }
         }
+        return null;
     }
 }
