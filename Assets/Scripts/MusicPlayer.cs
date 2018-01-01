dca59389c2b645fb376117cf9a090cbdc7ae3bde
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2018-01-02 02:13:12 +0700

    add background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        Debug.Log("Music player awake! " + GetInstanceID());
+
+        if (instance != null) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start () {
+        Debug.Log("Music player start " + GetInstanceID());
+	}
+	
+
+	void Update () {
+		
+	}
+}
